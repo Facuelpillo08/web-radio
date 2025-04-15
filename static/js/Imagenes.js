21d3480e7e434dccd97a3cdf49262b53c31999bc
Lautaro7089 <167309012+Lautaro7089@users.noreply.github.com>
2025-04-15 14:42:06 -0300

    Pagina inicio + js, css e img
    Pagina de inicio en proceso con las imagenes, css y js que se utilizan en la misma

---
+++ static/js/Imagenes.js
@@ -0,0 +1,221 @@
+const image = document.getElementById('foto1');
+
+// Cambiar la imagen cuando el cursor pasa por encima
+image.addEventListener('mouseover', function() {
+  // Hacemos que la imagen se desvanezca antes de cambiarla
+  image.style.opacity = 0;
+
+  // Esperamos un momento antes de cambiar la imagen para permitir el desvanecimiento
+  setTimeout(function() {
+    image.src = '../static/images/BerissoRetro.jpg';
+    // Volvemos a hacer visible la imagen con opacidad
+    image.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+// Volver a la imagen original cuando el cursor sale
+image.addEventListener('mouseout', function() {
+  image.style.opacity = 0;
+
+  setTimeout(function() {
+    image.src = '../static/images/berisso.jpg';
+    image.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+
+
+
+
+const image1 = document.getElementById('foto2');
+
+// Cambiar la imagen cuando el cursor pasa por encima
+image1.addEventListener('mouseover', function() {
+  // Hacemos que la imagen se desvanezca antes de cambiarla
+  image1.style.opacity = 0;
+
+  // Esperamos un momento antes de cambiar la imagen para permitir el desvanecimiento
+  setTimeout(function() {
+    image1.src = '../static/images/Casiweb.jpg';
+    // Volvemos a hacer visible la imagen con opacidad
+    image1.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+// Volver a la imagen original cuando el cursor sale
+image1.addEventListener('mouseout', function() {
+  image1.style.opacity = 0;
+
+  setTimeout(function() {
+    image1.src = '../static/images/casi.jpg';
+    image1.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+
+
+const image3 = document.getElementById('foto3');
+
+// Cambiar la imagen cuando el cursor pasa por encima
+image3.addEventListener('mouseover', function() {
+  // Hacemos que la imagen se desvanezca antes de cambiarla
+  image3.style.opacity = 0;
+
+  // Esperamos un momento antes de cambiar la imagen para permitir el desvanecimiento
+  setTimeout(function() {
+    image3.src = '../static/images/FuegoWeb.jpg';
+    // Volvemos a hacer visible la imagen con opacidad
+    image3.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+// Volver a la imagen original cuando el cursor sale
+image3.addEventListener('mouseout', function() {
+  image3.style.opacity = 0;
+
+  setTimeout(function() {
+    image3.src = '../static/images/fuego.jpg';
+    image3.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+
+const image4 = document.getElementById('foto7');
+
+// Cambiar la imagen cuando el cursor pasa por encima
+image4.addEventListener('mouseover', function() {
+  // Hacemos que la imagen se desvanezca antes de cambiarla
+  image4.style.opacity = 0;
+
+  // Esperamos un momento antes de cambiar la imagen para permitir el desvanecimiento
+  setTimeout(function() {
+    image4.src = '../static/images/GenteWeb.jpg';
+    // Volvemos a hacer visible la imagen con opacidad
+    image4.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+// Volver a la imagen original cuando el cursor sale
+image4.addEventListener('mouseout', function() {
+  image4.style.opacity = 0;
+
+  setTimeout(function() {
+    image4.src = '../static/images/gente.jpg';
+    image4.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+
+
+
+const image5 = document.getElementById('foto4');
+
+// Cambiar la imagen cuando el cursor pasa por encima
+image5.addEventListener('mouseover', function() {
+  // Hacemos que la imagen se desvanezca antes de cambiarla
+  image5.style.opacity = 0;
+
+  // Esperamos un momento antes de cambiar la imagen para permitir el desvanecimiento
+  setTimeout(function() {
+    image5.src = '../static/images/Hermosaweb.jpg';
+    // Volvemos a hacer visible la imagen con opacidad
+    image5.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+// Volver a la imagen original cuando el cursor sale
+image5.addEventListener('mouseout', function() {
+  image5.style.opacity = 0;
+
+  setTimeout(function() {
+    image5.src = '../static/images/hermosa.jpg';
+    image5.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+
+
+
+
+
+const image6 = document.getElementById('foto6');
+
+// Cambiar la imagen cuando el cursor pasa por encima
+image6.addEventListener('mouseover', function() {
+  // Hacemos que la imagen se desvanezca antes de cambiarla
+  image6.style.opacity = 0;
+
+  // Esperamos un momento antes de cambiar la imagen para permitir el desvanecimiento
+  setTimeout(function() {
+    image6.src = '../static/images/VayanWeb.jpg';
+    // Volvemos a hacer visible la imagen con opacidad
+    image6.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+// Volver a la imagen original cuando el cursor sale
+image6.addEventListener('mouseout', function() {
+  image6.style.opacity = 0;
+
+  setTimeout(function() {
+    image6.src = '../static/images/vayan.jpg';
+    image6.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+const image8 = document.getElementById('foto5');
+
+// Cambiar la imagen cuando el cursor pasa por encima
+image8.addEventListener('mouseover', function() {
+  // Hacemos que la imagen se desvanezca antes de cambiarla
+  image8.style.opacity = 0;
+
+  // Esperamos un momento antes de cambiar la imagen para permitir el desvanecimiento
+  setTimeout(function() {
+    image8.src = '../static/images/TallandoWeb.jpg';
+    // Volvemos a hacer visible la imagen con opacidad
+    image8.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+// Volver a la imagen original cuando el cursor sale
+image8.addEventListener('mouseout', function() {
+  image8.style.opacity = 0;
+
+  setTimeout(function() {
+    image8.src = '../static/images/tallando.jpg';
+    image8.style.opacity = 1;
+  }, 1000); // El mismo tiempo que la transición de opacidad
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
